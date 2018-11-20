1244091765386393db12128ec072f0bc65af09d9
Administrator <284477367@qq.com>
2018-11-20 09:41:30 +0800

    scala

---
+++ src/main/scala/collection/Amount.scala
@@ -0,0 +1,10 @@
+package collection
+
+
+ class Amount {
+
+
+  case class Dollar(value :Double) extends  Amount
+  case class Currency(value :Double,str:String) extends  Amount
+  case object Nothing extends Amount
+}
